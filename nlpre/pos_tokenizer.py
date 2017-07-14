15e2ef5a8c7c623d01523a9d2b657a73bb00ce2d
Travis Hoppe <travis.hoppe@gmail.com>
2017-07-14 00:54:04 -0400

    Upgrade pos_tokenizer to spacy with tests passing

--- nlpre/pos_tokenizer.py
+++ nlpre/pos_tokenizer.py
@@ -1,4 +1,4 @@
-#import pattern.en
+# import pattern.en
 import spacy
 from tokenizers import meta_text
 import logging
@@ -12,79 +12,121 @@ class pos_tokenizer(object):
     remove all words that are not nouns or adjectives. POS detection is
     provided by the spaCy.io.
 
+    See more here:
+    https://spacy.io/docs/usage/pos-tagging
+
     """
 
-    def __init__(self, POS_blacklist):
+    def __init__(self, blacklist):
         """
         Initialize the parser.
 
         Args:
-            POS_blacklist: A list of parts of speech to remove from the text.
+            blacklist: A list of parts of speech to remove from the text.
         """
         self.logger = logging.getLogger(__name__)
         self.nlp = spacy.load('en')
-        self.set_blacklist(POS_blacklist)
+        self.set_blacklist(blacklist)
 
+    def set_blacklist(self, blacklist):
 
-    def set_blacklist(self, POS_blacklist):
-
-        POS = {
-            "noun"         : ["NOUN",],
-            "proper_noun"  : ["PROPN",],
-            "pronoun"      : ["PRON",],
-            "adjective"    : ["ADJ"],
-            "verb"         : ["VERB"],
-            "adverb"       : ["ADV",],
-            "connector"    : ["CCONJ", "CONJ", "DET", "ADP", "INTJ","PRON"],
-            "punctuation"  : ["PUNCT", "NUM", "PART",],
-            "symbol"       : ["SYM",],
-            "unknown"      : ["X"],
+        self.POS_map = {
+            "-LRB-": "punctuation",
+            "-PRB-": "punctuation",
+            ",": "punctuation",
+            ":": "punctuation",
+            ".": "punctuation",
+            "''": "quote",
+            "": "quote",
+            "#": "symbol",
+            "``": "quote",
+            "$": "symbol",
+            "ADD": "email",
+            "AFX": "affix",
+            "BES": "common_verb",
+            "CC": "connector",
+            "CD": "number",
+            "DT": "determiner",
+            "EX": "common_verb",
+            "FW": "unknown",
+            "GW": "unknown",
+            "HVS": "common_verb",
+            "HYPH": "symbol",
+            "IN": "connector",
+            "JJ": "adjective",
+            "JJR": "adjective",
+            "JJS": "adjective",
+            "LS": "symbol",
+            "MD": "common_verb",
+            "NFP": "punctuation",
+            "NIL": "unknown",
+            "NN": "noun",
+            "NNP": "noun",
+            "NNPS": "noun",
+            "NNS": "noun",
+            "PDT": "common_verb",
+            "POS": "possessive_ending",
+            "PRP": "pronoun",
+            "PRP$": "pronoun",
+            "RB": "adverb",
+            "RBR": "adverb",
+            "RBS": "adverb",
+            "RP": "adverb",
+            "SP": "space",
+            "SYM": "symbol",
+            "TO": "common_verb",
+            "UH": "connector",
+            "VB": "verb",
+            "VBD": "verb",
+            "VBG": "verb",
+            "VBN": "verb",
+            "VBP": "verb",
+            "VBZ": "verb",
+            "WDT": "common_verb",
+            "WP": "common_verb",
+            "WP$": "common_verb",
+            "WRB": "common_verb",
+            "XX": "unknown",
         }
-        
-        self.filtered_POS = POS_blacklist
-        self.POS_map = {}
-        for pos, L in POS.items():
-            for y in L:
-                self.POS_map[y] = pos
+
+        self.blacklist = blacklist
 
         return self
 
-    def __call__(self, text, force_lemma=True):
+    def __call__(self, text, lemmatize=True):
         '''
         Runs the parser.
 
         Args:
             text: a string document
-            force_lemma: bool, lemmitze the words prior to parsing
+            lemmatize: bool, lemmatize the words prior to parsing
         Returns:
             results: A string document
         '''
 
         # Due to spaCy.ios handling of hyphens, we need to mask them beforehand
         text = text.replace('-', '_HYPHENNN')
-        proc_text = self.nlp(text)
 
         pos_tags = []
         doc2 = []
         removedWords = []
-        
+
         for sent in self.nlp(text).sents:
 
             sent2 = []
             for token in sent:
 
-                
                 # Skip extra spaces
                 if token.is_space:
                     continue
 
                 # There are so many different pronouns in spaCy, if we find one
                 # and we don't want it, remove it here
-                if token.lemma_ == "-PRON-" and "pronoun" in self.filtered_POS:
+                if token.lemma_ == "-PRON-" and "pronoun" in self.blacklist:
                     removedWords.append(token.text)
                     continue
-                
-                if force_lemma:
+
+                if lemmatize:
                     num_caps = sum((x.isupper() for x in token.text))
 
                     # For words with multiple caps, don't lemmatize
@@ -99,89 +141,29 @@ class pos_tokenizer(object):
                     # Otherwise lemmatize
                     else:
                         word = token.lemma_
-                        
+
                 else:
                     word = token.text
 
-                
                 # Words with underscores are marked as nouns
                 if "_" in word:
                     pos = "noun"
                 else:
-                    pos = self.POS_map[token.pos_]
+                    pos = self.POS_map[token.tag_]
 
-                if pos in self.filtered_POS:
+                if pos in self.blacklist:
                     removedWords.append(word)
                     continue
 
                 sent2.append(word)
                 pos_tags.append(pos)
-            
+
             doc2.append(' '.join(sent2))
 
         doc2 = '\n'.join(doc2)
 
         # Undo the spaCy.io hack
         doc2 = doc2.replace('_HYPHENNN', '-')
-        
-        self.logger.info('Removed words: %s' % removedWords)
-
-        # The number of POS tokens should match the number of word tokens
-        assert(len(pos_tags) == len(doc2.split()))
-
-        result = meta_text(doc2, POS=pos_tags)
-        return result
-
-        '''
-        exit()
-
-        pos_tags = []
-        tokens = self.parse(text)
-        doc2 = []
-        removedWords = []
-
-        for sentence in tokens.split():
-            sent2 = []
-
-            for word, tag in sentence:
-
-                if "PHRASE_" in word:
-                    sent2.append(word)
-                    pos_tags.append(_POS_shorthand["noun"])
-                    continue
-
-                if "MeSH_" in word:
-                    sent2.append(word)
-                    pos_tags.append(_POS_shorthand["noun"])
-                    continue
-
-                tag = tag.split('|')[0].split('-')[0].split("&")[0]
-
-                # try:
-                #    pos = self.POS_map[tag]
-                # except BaseException:
-                #    self.logger.info("UNKNOWN POS *{}*".format(tag))
-                #    pos = "unknown"
-
-                pos = self.POS_map[tag]
-
-                if pos in self.filtered_POS:
-                    removedWords.append(word)
-                    continue
-
-                word = pattern.en.singularize(word, pos)
-
-                if pos == "verb" or force_lemma:
-                    lem = pattern.en.lemma(word, parse=False)
-                    if lem is not None:
-                        word = lem
-
-                sent2.append(word)
-                pos_tags.append(_POS_shorthand[pos])
-
-            doc2.append(' '.join(sent2))
-
-        doc2 = '\n'.join(doc2)
 
         self.logger.info('Removed words: %s' % removedWords)
 
@@ -190,4 +172,3 @@ class pos_tokenizer(object):
 
         result = meta_text(doc2, POS=pos_tags)
         return result
-        '''
--- tests/pos_tokenizer_tests.py
+++ tests/pos_tokenizer_tests.py
@@ -1,13 +1,17 @@
 from nose.tools import *
 from nlpre import pos_tokenizer
 
-base_blacklist = [
+noun_blacklist = [
     "connector",
     "pronoun",
-    "adverb",
     "symbol",
-    "verb",
     "punctuation",
+    "determiner",
+    "possessive_ending",
+    "common_verb",
+    "verb",
+    "adverb",
+    "number",
 ]
 
 class POS_Tokenizer_Test:
@@ -19,79 +23,72 @@ class POS_Tokenizer_Test:
     def keep_nouns_test1(self):
         doc = u"The boy threw the ball into the yard"
         doc_right = u"boy ball yard"
-        doc_new = self.parser.set_blacklist(base_blacklist)(doc)
+        doc_new = self.parser.set_blacklist(noun_blacklist)(doc)
 
         assert_equal(doc_right, doc_new.text)
 
     def keep_nouns_test2(self):
         doc = u"So we beat on, boats against the current, borne back ceaselessly into the past"
         doc_right = u"boat current past"
-        doc_new = self.parser.set_blacklist(base_blacklist)(doc)
+        doc_new = self.parser.set_blacklist(noun_blacklist)(doc)
 
         assert_equal(doc_right, doc_new.text)
 
     def keep_nouns_test3(self):
         doc = u" A screaming comes across the sky. It has happened before, but there is nothing to compare it to now."
         doc_right = u"screaming sky\nnothing"
-        doc_new = self.parser.set_blacklist(base_blacklist)(doc)
+        doc_new = self.parser.set_blacklist(noun_blacklist)(doc)
 
         assert_equal(doc_right, doc_new.text)
 
     def keep_nouns_test4(self):
         doc = (u"Many years later, as he faced the firing squad, he was to remember that distant"
                u" afternoon when his father took him to discover ice")
-        doc_right = u"Many year fire squad distant afternoon father ice"
-        doc_new = self.parser.set_blacklist(base_blacklist)(doc)
+        doc_right = u"Many year firing squad distant afternoon father ice"
+        doc_new = self.parser.set_blacklist(noun_blacklist)(doc)
 
         assert_equal(doc_right, doc_new.text)
 
     def keep_nouns_test5(self):
         doc = u"The sky above the port was the color of television, tuned to a dead channel"
         doc_right = u"sky port color television dead channel"
-        doc_new = self.parser.set_blacklist(base_blacklist)(doc)
+        doc_new = self.parser.set_blacklist(noun_blacklist)(doc)
 
         assert_equal(doc_right, doc_new.text)
 
     def keep_nouns_test6(self):
         doc = (u"In my younger and more vulnerable years my father gave me some advice that "
                u"I've been turning over in my mind ever since")
-        doc_right = u"younger vulnerable year father advice mind"
-        doc_new = self.parser.set_blacklist(base_blacklist)(doc)
+        doc_right = u"young vulnerable year father advice mind"
+        doc_new = self.parser.set_blacklist(noun_blacklist)(doc)
 
         assert_equal(doc_right, doc_new.text)
 
     def keep_mesh_test(self):
         doc = u"The boy MeSH_Threw the ball into the yard"
         doc_right = u"boy MeSH_Threw ball yard"
-        doc_new = self.parser.set_blacklist(base_blacklist)(doc)
+        doc_new = self.parser.set_blacklist(noun_blacklist)(doc)
 
         assert_equal(doc_right, doc_new.text)
 
     def keep_phrase_test(self):
         doc = u"The boy PHRASE_Threw the ball into the yard"
         doc_right = u"boy PHRASE_Threw ball yard"
-        doc_new = self.parser.set_blacklist(base_blacklist)(doc)
+        doc_new = self.parser.set_blacklist(noun_blacklist)(doc)
 
         assert_equal(doc_right, doc_new.text)
 
     def possesive_word_test(self):
         doc = u"I am Jack's complete lack of surprise"
-        doc_right = u"be jack complete lack of surprise"
-        doc_new = self.parser.set_blacklist(["pronoun"])(doc)
+        doc_right = u"I be Jack complete lack of surprise"
+        doc_new = self.parser.set_blacklist(["possessive_ending"])(doc)
 
         assert_equal(doc_right, doc_new.text)
 
-    def cardinal_word_test(self):
-        doc = u"There are two phases."
-        doc_right = u"There be phase ."
-        doc_new = self.parser.set_blacklist(["punctuation"])(doc)
-        
-        assert_equal(doc_right, doc_new.text)
-
-    def w_word_test(self):
+    def common_verb_test(self):
         doc = u"Transcriptions that are observed."
         doc_right = u"Transcription be observe ."
-        doc_new = self.parser.set_blacklist(["w_word"])(doc)
+        doc_new = self.parser.set_blacklist(["common_verb"])(doc)
         
         assert_equal(doc_right, doc_new.text)
 
@@ -108,9 +105,24 @@ class POS_Tokenizer_Test:
         doc_new = self.parser.set_blacklist(["symbol"])(doc)
 
         assert_equal(doc_right, doc_new.text)
+        
+    def number_test(self):
+        doc = u'''We are number one and twenty one.'''
+        doc_right = u"We be number and ."
+        doc_new = self.parser.set_blacklist(["number"])(doc)
 
-    # def unknown_word_test(self):
-    #    doc = 'The boy akjf45!naf the ball into the yard'
-    #    doc_right = 'boy akjfnaf ball yard'
-    #    doc_new = self.parser(doc)
-    #    assert_equal(doc_right, doc_new.text)
+        assert_equal(doc_right, doc_new.text)
+
+    def lemmatize_test(self):
+        doc = u'''I am a superstar.'''
+
+        doc_right = u"I be a superstar ."
+        doc_new = self.parser.set_blacklist([])(doc, lemmatize=True)
+        assert_equal(doc_right, doc_new.text)
+
+        doc_right = u"I am a superstar ."
+        doc_new = self.parser.set_blacklist([])(doc, lemmatize=False)
+        assert_equal(doc_right, doc_new.text)
+
+        
+
